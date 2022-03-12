385d9067c818e791dfd8fd2bb74c305415eef4aa
Filip Rybczyński <filipryba@gmail.com>
2022-03-12 23:24:54 +0100

    Changed way displayed text is cleared - escaped strings array is now simply replaced with an empty one;

--- src/components/Generator.js
+++ src/components/Generator.js
@@ -22,7 +22,6 @@ export default class Generator extends React.Component {
       toneSpecificPhrases: {}, // object of arrays
       // property to control whether text is generated in the Display component
       // this way escapedStrings can remain in state even when display is cleared
-      displayText: true,
     };
   }
 
@@ -53,7 +52,7 @@ export default class Generator extends React.Component {
 
   // Function that will prevent the escapedString array to be passed on to the Display component and, as a result, no text will be displayed (as generation is dependent on there being props.children)
   handleClearing = () => {
-    this.setState({displayText: false})
+    this.setState({escapedStrings: []})
   }
 
   render() {
@@ -67,7 +66,7 @@ export default class Generator extends React.Component {
           updateEscapedStrings={this.updateEscapedStrings}
         />
         <Display handleClearing={this.handleClearing}>
-          {this.state.displayText && this.state.escapedStrings}
+          {this.state.escapedStrings}
           {/* Alright, let me tell you about escaping (while async loads)
 
 HTML escaping is a bit funny, but not much funnier than writing how to do it. Say I want to use the symbol < in a sentence. That's quite easy to do: I just write "&lt;". However, in order to write "&lt;", I need to write "&quot;&amp;lt;&quot;". Then in order to write that, I need to
