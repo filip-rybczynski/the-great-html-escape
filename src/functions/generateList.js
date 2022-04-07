bec2b5a050957816cc3bd30e4966d1af43470b0c
Filip Rybczyński <filipryba@gmail.com>
2022-04-07 20:28:52 +0200

    JS: Created generateList function

---
+++ src/functions/generateList.js
@@ -0,0 +1,14 @@
+export default function generateList(escapedStrings) {
+  return (
+    <ul>
+      {escapedStrings.map((string, i, arr) => (
+        <li key={string}>
+          <code>{string}</code>
+          {/* Adds tag if it's not the last list item (slightly different for the first item) */}
+          {i < arr.length - 1 && i > 0 && ", which"}
+          {i < arr.length - 1 && " escapes to"}
+        </li>
+      ))}
+    </ul>
+  );
+}
