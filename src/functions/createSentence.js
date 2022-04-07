641031f4084627fc57e93833c3fd5b568772ac92
Filip Rybczyński <filipryba@gmail.com>
2022-04-07 19:51:47 +0200

    CSS: basic styling completed

--- src/functions/createSentence.js
+++ src/functions/createSentence.js
@@ -2,8 +2,8 @@ export default function createSentence(firstString, secondString, phrases) {
   console.log(firstString, secondString, phrases);
   let sentence = (
     <span>
-      {phrases[0]} <code style={{ color: "red" }}>{firstString}</code>{" "}
-      {phrases[1]} <code style={{ color: "red" }}>{secondString}</code>
+      {phrases[0]} <code >{firstString}</code>{" "}
+      {phrases[1]} <code >{secondString}</code>
       {phrases.length === 3 ? phrases[2] : null}
     </span>
   );
