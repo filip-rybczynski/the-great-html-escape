adbae20df3828b96d5f31276a8926c63b1caea5d
Filip Rybczyński <filipryba@gmail.com>
2022-04-11 20:48:14 +0200

    Display list - custom bullet points

--- src/functions/generateList.js
+++ src/functions/generateList.js
@@ -2,7 +2,8 @@ export default function generateList(escapedStrings) {
   return (
     <ul>
       {escapedStrings.map((string, i, arr) => (
-        <li key={string}>
+        <li key={i}> 
+        {/* order of elements does not change so OK to use index as key */}
           <code>{string}</code>
           {/* Adds tag if it's not the last list item (slightly different for the first item) */}
           {i < arr.length - 1 && i > 0 && ", which"}
